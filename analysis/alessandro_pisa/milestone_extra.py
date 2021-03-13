44cae22d518f2b2f02045c2c74bb6a8765ebb64c
AlePPisa <aleppaulon@gmail.com>
2021-03-13 13:09:16 -0600

    Finished data loading and preprocessing, saving for backup

---
+++ analysis/alessandro_pisa/milestone_extra.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+
+def load_and_process_data(directory):
+    df = pd.read_csv(directory) # I can't chain this one because I access df right here
+    df = (df.rename(columns = {key: key.replace('-',' ').title() for key in df.columns}) # <---- 
+        .replace({'Class': {'e':'edible', 'p': 'poisonous'},
+            'Cap Shape': {'b': 'bell', 'c': 'conical', 'x': 'convex', 'f': 'flat', 'k': 'knobbed', 's': 'sunken'},
+            'Cap Surface': {'f': 'fibrous', 'g': 'grooves', 'y' : 'scaly', 's': 'smooth'},
+            'Cap Color': {'n': 'brown', 'b':'buff','c':'cinnamon','g':'gray','r':'green','p':'pink','u':'purple','e': 'red','w':'white','y':'yellow'},
+            'Bruises': {'t': True, 'f': False},
+            'Odor': {'a': 'almond', 'l':'anise','c':'creosote','y':'fishy','f':'foul','m':'musty','n':'none','p':'pungent','s':'spicy'},
+            'Gill Attachment': {'a':'attached', 'd':'descending','f':'free','n':'notched'},
+            'Gill Spacing': {'c':'close','w':'crwoded','d':'distant'},
+            'Gill Size': {'b':'broad','n':'narrow'},
+            'Gill Color': {'k': 'black', 'n':'brown','b':'buff','h':'chocolate','g':'gray','r':'green','o':'orange','p':'pink','u':'purple','e':'red','w':'white','y':'yellow'},
+            'Stalk Shape': {'e':'enlarging','t':'tapering'},
+            'Stalk Root': {'b':'bulbous','c':'club','u':'cup','e':'equal','z':'rhizomorphs','r':'rooted','?':np.nan},
+            'Stalk Surface Above Ring': {'f':'fibrous','y':'scaly','k':'silky','s':'smooth'},
+            'Stalk Surface Below Ring': {'f':'fibrous','y':'scaly','k':'silky','s':'smooth'},
+            'Stalk Color Above Ring': {'n':'brown','b':'buff','c':'cinnamon','g':'gray','o':'orange','p':'pink','e':'red','w':'white','y':'yellow'},
+            'Stalk Color Below Ring': {'n':'brown','b':'buff','c':'cinnamon','g':'gray','o':'orange','p':'pink','e':'red','w':'white','y':'yellow'},
+            'Veil Type': {'p':'partial','u':'universal'},
+            'Veil Color': {'n':'brown','o':'orange','w':'white','y':'yellow'},
+            'Ring Number': {'n':0,'o':1,'t':2},
+            'Ring Type': {'c':'cobwebby','e':'evanescent','f':'flaring','l':'large','n':'none','p':'pendant','s':'sheathing','z':'zone'},
+            'Spore Print Color': {'k':'black','n':'brown','b':'buff','h':'chocolate','r':'green','o':'orange','u':'purlple','w':'white','y':'yellow'},
+            'Population': {'a':'abundant','c':'clustered','n':'numerous','s':'scattered','v':'several','y':'solitary'},
+            'Habitat': {'g':'grasses','l':'leaves','m':'meadows','p':'paths','u':'urban','w':'waste','d':'woods'}})
+        .sort_values(by='Class').reset_index().drop('index',axis=1))
+    return df
