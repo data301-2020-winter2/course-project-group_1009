6621e2830706da6b49af0508459a5999bd991fdf
ullattil <60049652+ullattil@users.noreply.github.com>
2021-04-12 12:09:09 -0700

    Abijith: added the ratioDict as a csv file in the repository for use in tableau

--- analysis/abijith_ashok/Scripts/function.py
+++ analysis/abijith_ashok/Scripts/function.py
@@ -28,7 +28,8 @@ def load_processed(srcpath, despath):
             'Ring Type': {'c':'cobwebby','e':'evanescent','f':'flaring','l':'large','n':'none','p':'pendant','s':'sheathing','z':'zone'},
             'Spore Print Color': {'k':'black','n':'brown','b':'buff','h':'chocolate','r':'green','o':'orange','u':'purlple','w':'white','y':'yellow'},
             'Population': {'a':'abundant','c':'clustered','n':'numerous','s':'scattered','v':'several','y':'solitary'},
-            'Habitat': {'g':'grasses','l':'leaves','m':'meadows','p':'paths','u':'urban','w':'waste','d':'woods'}})
+            'Habitat': {'g':'grasses','l':'leaves','m':'meadows','p':'paths','u':'urban','w':'waste','d':'woods'}}) .assign(Edible=data['class']).replace({'Edible':{'e':True, 'p':False}}).sort_values(by='Class').reset_index().drop('index',axis=1)
+    return data
 
 #    dict = {'Class': {'e':'edible', 'p': 'poisonous'},
 #           'Cap Shape': {'b': 'bell', 'c': 'conical', 'x': 'convex', 'f': 'flat', 'k': 'knobbed', 's': 'sunken'},
@@ -78,6 +79,41 @@ def vennD(colname, colval, colname1, colval1, edibility, data ):
     df2=data[(data[colname]==colval) & (data[colname1]==colval1) & (data['Class']==edibility)]
     df3=data[(data[colname1]==colval1) & (data['Class']==edibility)]
     return venn2(subsets = (len(df1)-len(df2), len(df3)-len(df2), len(df2)), set_labels = (colval+" "+colname, colval1+" "+colname1) )
+
+## function made my Ale Pisa and Alec Nixon
+def show_edibility_ratio(df):
+    ratioDict = {}
+
+    length = len(df.columns[1:])
+    #axes = [None]*length
+    #h = plt.figure(figsize=(10*3,10*int(length/3+1)))
+    for i in range(1, length):
+        col = df.columns[i]
+        rqData = pd.DataFrame(df[col].value_counts())
+        rqData.reset_index(inplace = True)
+
+        rqData = rqData.merge(pd.DataFrame(df[col].loc[df['Edible'] != True].value_counts()).reset_index().rename(columns = {col:'Not Edible'}), how='outer')
+        rqData = rqData.merge(pd.DataFrame(df[col].loc[df['Edible']].value_counts()).reset_index().rename(columns = {col:'Edible'}), how='outer')
+        rqData['Edibility'] = rqData['Edible'].fillna(0) / (rqData['Not Edible'].fillna(0) + rqData['Edible'].fillna(0))
+        #print(rqData)
+        ratioDict.update({col:rqData})
+        #axes[i] = h.add_subplot(int(length/3+1),3,i)
+        #sns.barplot(data=rqData, x='index', y='Edibility')
+    return ratioDict
+def sort_by_influence(ratioDict, keepCoeff=False):
+    ratios = pd.DataFrame(columns=['Feature','Edibility'])
+    for ratio in ratioDict.values():
+        for index, row in ratio.iterrows():
+            #nr={'Feature':'{} {}'.format(row['index'],ratio.columns[1]),'Edibility':row['Edibility']}
+            nr={'Feature':'{} {}'.format(regex.sub(r'([a-z])([a-z]*)',lambda match: '{}{}'.format(match.group(1).upper(),match.group(2)),str(row['index'])),ratio.columns[1]),'Edibility':row['Edibility']}
+            #print(nr)
+            ratios = ratios.append(nr, ignore_index=True)
+    ratios['coeff'] = abs(ratios['Edibility']-0.5)+0.5
+    ratios.sort_values(by=['coeff'],ascending=False).reset_index(drop=True,inplace=True)
+    if not keepCoeff:
+        ratios.drop(['coeff'],axis=1,inplace=True)
+    return ratios
+
 # Use the venn2 function
 #venn2(subsets = (10, 5, 2), set_labels = ('Group A', 'Group B'))
 #plt.show()
